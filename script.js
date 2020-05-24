2de2ed2586dbffa21d51857ac00b63bdfa715d42
Jeffrey Fisher <jeff@slofish.io>
2020-05-23 21:33:03 -0700

    Simplify code and make it block before first scroll.

--- script.js
+++ script.js
@@ -1,31 +1,46 @@
 function removePromotions() {
-    let promotedTweet = $('article:has(svg ~ div:contains("Promoted"))');
-    if (promotedTweet) {
-        promotedTweet.css('display', 'none');
-    }
+  let promotedTweet = $('article:has(svg ~ div:contains("Promoted"))');
+  if (promotedTweet) {
+    promotedTweet.css('display', 'none');
+  }
 
-    let trending = $('div[aria-label*="Timeline: Trending now"]>div>div');
-    trending.each(function(i) {
-        let promotedTrend = $(this).find('svg~span>span:contains("Promoted")');
-        if (promotedTrend.length) {
-            console.log('Hiding Promoted Trend');
-            $(this).css('display', 'none')
-        }
-    });
+  let trendingPromotion = $(
+    'div[aria-label*="Timeline: Trending now"]>div>div:has(svg~span>span:contains("Promoted"))'
+  );
+  if (trendingPromotion) {
+    trendingPromotion.css('display', 'none');
+  }
 
-    let whoToFollow = $('aside[aria-label*="Who to follow"]>div>div');
-    whoToFollow.each(function(i) {
-        let promotedTweeter = $(this).find('svg~div>span:contains("Promoted")');
-        if (promotedTweeter.length) {
-            console.log('Hiding Promoted Tweeter');
-            $(this).css('display', 'none')
-        }
-    });
-} 
+  let whoToFollowPromotion = $(
+    'aside[aria-label*="Who to follow"]>div>div:has(svg~div>span:contains("Promoted"))'
+  );
+  if (whoToFollowPromotion) {
+    whoToFollowPromotion.css('display', 'none');
+  }
+}
 
+// Remove promotions on every scroll
 window.addEventListener('scroll', removePromotions);
 
-$(function() {
-    removePromotions();
-});
+// Make sure the promotions are removed on the first page load.
+// Will remove every 500ms for up to the first 10 seconds.
+setTimeout(removePromotions, 500);
+setTimeout(removePromotions, 1000);
+setTimeout(removePromotions, 1500);
+setTimeout(removePromotions, 2000);
+setTimeout(removePromotions, 2500);
+setTimeout(removePromotions, 3000);
+setTimeout(removePromotions, 3500);
+setTimeout(removePromotions, 4000);
+setTimeout(removePromotions, 4500);
+setTimeout(removePromotions, 5500);
+setTimeout(removePromotions, 6000);
+setTimeout(removePromotions, 6500);
+setTimeout(removePromotions, 7000);
+setTimeout(removePromotions, 7500);
+setTimeout(removePromotions, 8000);
+setTimeout(removePromotions, 8500);
+setTimeout(removePromotions, 9000);
+setTimeout(removePromotions, 9500);
+setTimeout(removePromotions, 10000);
 
